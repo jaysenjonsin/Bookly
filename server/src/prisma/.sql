5d5c1966a1af7740152c1c94c3de72b81323c20f
jaysenjonsin <100107689+jaysenjonsin@users.noreply.github.com>
2023-02-01 00:46:45 -0800

    Db relations (#1)
    * add some session data
    
    * create follower/following relationship
    
    * finish tables

--- server/src/prisma/.sql
+++ server/src/prisma/.sql
@@ -1,11 +1,24 @@
 -- prisma sql equivalents
+--note: when defining foreign and primary key, make sure to put the key in ()  ex. FOREIGN KEY ("user_id"). also use () when defining references ex. "social"."users"("id")
 
 -- create user table
 -- note: by default, primary key is already indexed in postgres
 CREATE TABLE "social"."users" ("id" serial NOT NULL,"username" varchar(45) NOT NULL UNIQUE,"email" varchar(45) NOT NULL UNIQUE,"password" varchar(500) NOT NULL,"name" varchar(50) NOT NULL,"cover_pic" varchar(200),"profile_pic" varchar(200),"city" varchar(50),"website" varchar(50),  "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "updatedAt" TIMESTAMP(3) NOT NULL, PRIMARY KEY ("id"));
 
-
+--type "serial" in postgres applies autoincrement
 
 -- create posts table
 CREATE TABLE "social"."posts" ("id" serial NOT NULL, "desc" varchar(200), "img" varchar(200), "user_id" INT, PRIMARY KEY ("id"), CONSTRAINT "user_id" FOREIGN KEY ("user_id") REFERENCES "social"."users" ("id") ON DELETE CASCADE ON UPDATE CASCADE);
+
+-- create comments table
+CREATE TABLE "social"."comments" ("id" serial NOT NULL, "desc" varchar(200) NOT NULL, "img" varchar(200), "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP, "updated_at" TIMESTAMP(3) NOT NULL,  "user_id" INT NOT NULL, "post_id" INT NOT NULL, CONSTRAINT "user_id" FOREIGN KEY ("user_id") REFERENCES "social"."users" ("id") ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT "post_id" FOREIGN KEY "post_id" REFERENCES "social"."posts" ("id") ON DELETE CASCADE ON UPDATE CASCADE);
+
+--create stories table
+CREATE "social"."stories"("id" serial NOT NULL, "img" varchar(400) NOT NULL, "user_id" INT NOT NULL, PRIMARY KEY ("id"), CONSTRAINT "user_id" FOREIGN KEY ("user_id") REFERENCES "social"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE);
+
+--create relationships table - join table for "User" to "User" many to many relationship
+CREATE TABLE "social"."relationships"("id" serial NOT NULL, "follower_user_id" INT NOT NULL, "followed_user_id" INT NOT NULL, PRIMARY KEY("id"), CONSTRAINT "follower_user" FOREIGN KEY("follower_user_id") REFERENCES "social"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT "follwed_user" FOREIGN KEY("followed_user_id") REFERENCES "social"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE);
+
+--create likes table
+CREATE TABLE "social"."likes"("id" serial NOT NULL, "user_id" INT NOT NULL, "post_id" NOT NULL, PRIMARY KEY("id"), CONSTRAINT "user_id" FOREIGN KEY("user_id") REFERENCES "social"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT "post_id" FOREIGN KEY("post_id") REFERENCES "social"."posts"("id") ON DELETE CASCADE ON UPDATE CASCADE);
---
+++ server/src/prisma/migrations/20230201083001_add_tables/migration.sql
@@ -0,0 +1,100 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "social";
+
+-- CreateTable
+CREATE TABLE "social"."User" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(45) NOT NULL,
+    "password" VARCHAR(400) NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+    "cover_pic" VARCHAR(200),
+    "profile_pic" VARCHAR(200),
+    "city" VARCHAR(50),
+    "website" VARCHAR(50),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "social"."Post" (
+    "id" SERIAL NOT NULL,
+    "desc" VARCHAR(400) NOT NULL,
+    "img" VARCHAR(400),
+    "user_id" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "social"."Comment" (
+    "id" SERIAL NOT NULL,
+    "desc" VARCHAR(400) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "post_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "social"."Story" (
+    "id" SERIAL NOT NULL,
+    "img" VARCHAR(400) NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Story_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "social"."Relationship" (
+    "id" SERIAL NOT NULL,
+    "follower_user_id" INTEGER NOT NULL,
+    "followed_user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Relationship_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "social"."Like" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "post_id" INTEGER NOT NULL,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "social"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "social"."User"("email");
+
+-- AddForeignKey
+ALTER TABLE "social"."Post" ADD CONSTRAINT "Post_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "social"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social"."Comment" ADD CONSTRAINT "Comment_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "social"."Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social"."Comment" ADD CONSTRAINT "Comment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "social"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social"."Story" ADD CONSTRAINT "Story_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "social"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social"."Relationship" ADD CONSTRAINT "Relationship_follower_user_id_fkey" FOREIGN KEY ("follower_user_id") REFERENCES "social"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social"."Relationship" ADD CONSTRAINT "Relationship_followed_user_id_fkey" FOREIGN KEY ("followed_user_id") REFERENCES "social"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social"."Like" ADD CONSTRAINT "Like_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "social"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "social"."Like" ADD CONSTRAINT "Like_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "social"."Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;

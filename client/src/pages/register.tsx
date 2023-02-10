1a37a4ccad2543122ad830dd74ca8bfdc36db3f3
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-02-09 16:30:39 -0800

    basic form validation

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -5,16 +5,30 @@ import { zodResolver } from '@hookform/resolvers/zod'; //from @hookform/resolver
 
 type Props = {};
 
-const formData = z.object({
-  name: z.string({ invalid_type_error: 'Please enter your name' }),
-  email: z.string({ invalid_type_error: 'Please enter your email' }).email(),
-  username: z.string({ invalid_type_error: 'Please enter a username' }),
-  password: z.string({ invalid_type_error: 'Please enter a password' }).min(3),
-  confirmPassword: z.string({ invalid_type_error: 'Please confirm password' }),
-});
+const formData = z
+  .object({
+    name: z.string().min(1, { message: 'Name required' }),
+    email: z
+      .string()
+      .min(1, { message: 'Email required' })
+      .email({ message: 'please enter a valid email' }),
+    username: z.string().min(1, { message: 'Username required' }),
+    password: z.string().min(1, { message: 'password required' }),
+    confirmPassword: z.string({
+      invalid_type_error: 'Please confirm password',
+    }),
+  })
+  //custom validation logic using .refine
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Passwords do no match',
+  });
 
-const postRequest = async () => {
-  console.log('send post request to backend');
+type formDataType = z.infer<typeof formData>;
+
+const onSubmit: SubmitHandler<formDataType> = (data) => {
+  //SEND POST REQUEST TO BACKEND
+  console.log(data);
 };
 
 const Register = (props: Props) => {
@@ -22,18 +36,65 @@ const Register = (props: Props) => {
     register,
     handleSubmit,
     formState: { errors, isSubmitting },
-  } = useForm({
+  } = useForm<formDataType>({
     resolver: zodResolver(formData),
   });
 
   return (
     <>
       <div>Register</div>
-      <form onSubmit={handleSubmit(postRequest)}>
-        <input type='text' disabled={isSubmitting} {...register('name')} /> 
-        <input type='text' disabled={isSubmitting} />
-        <input type='text' disabled={isSubmitting} />
-        <input type='text' disabled={isSubmitting} />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          id='name'
+          type='text'
+          className='input'
+          placeholder='name'
+          disabled={isSubmitting} //cannot change when form is submitting
+          {...register('name')} //gives some props to (i.e registers) the named input: ex ref = {name} name = {name} onChange = {onchange}
+        />
+        <p className='error-message'>{errors.name?.message}</p>
+
+        <input
+          id='email'
+          type='text'
+          className='input'
+          placeholder='email'
+          disabled={isSubmitting}
+          {...register('email')}
+        />
+        <p className='error-message'>{errors.email?.message}</p>
+
+        <input
+          id='username'
+          type='text'
+          className='input'
+          placeholder='username'
+          disabled={isSubmitting}
+          {...register('username')}
+        />
+        <p className='error-message'>{errors.username?.message}</p>
+
+        <input
+          id='password'
+          type='text'
+          className='input'
+          placeholder='password'
+          disabled={isSubmitting}
+          {...register('password')}
+        />
+        <p className='error-message'>{errors.password?.message}</p>
+
+        <input
+          id='confirmPassword'
+          type='text'
+          className='input'
+          placeholder='confirm password'
+          disabled={isSubmitting}
+          {...register('confirmPassword')}
+        />
+        <p className='error-message'>{errors.confirmPassword?.message}</p>
+
+        <button type='submit'>submit</button>
       </form>
     </>
   );

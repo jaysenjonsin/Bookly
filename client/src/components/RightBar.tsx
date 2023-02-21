51ef530927395e9c5a4d746031b857e8883cf7dd
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-02-20 16:43:16 -0800

    basic right bar styling

--- client/src/components/RightBar.tsx
+++ client/src/components/RightBar.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import styles from '@/styles/RightBar.module.scss';
-type Props = {};
+import Image from 'next/image';
+import profilePic from '../../public/undraw_Reading_book_re_kqpk.png';
+// type Props = {};
 
-const RightBar = (props: Props) => {
+const RightBar = (props: {}) => {
   return (
     <>
-      <div className={styles.rightBar}>right bar</div>
+      <div className={styles.rightBar}>
+        <div className={styles.container}>
+          <div className={styles.item}>
+            <span>Suggestions</span>
+            <div className={styles.user}>
+              <div className={styles.userInfo}>
+                <Image src={profilePic} alt='profile pic' />
+                <span>user's name</span>
+              </div>
+              <div className={styles.buttons}></div>
+              <button>follow</button>
+              <button>X</button>
+            </div>
+            <div className={styles.user}>
+              <div className={styles.userInfo}>
+                <Image src={profilePic} alt='profile pic' />
+                <span>user's name</span>
+              </div>
+              <div className={styles.buttons}></div>
+              <button>follow</button>
+              <button>X</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };

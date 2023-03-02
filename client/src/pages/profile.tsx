81863c382e1076c9af988b945b4e5bb152fa6ac5
jaysenjonsin <jasoncjohnson5@yahoo.com>
2023-03-01 23:44:53 -0800

    start styling profile

--- client/src/pages/profile.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = {};
-
-const profile = (props: Props) => {
-  return <div>profile</div>;
-};
-
-export default profile;
---
+++ client/src/pages/profile/[id].tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import s from '@/styles/profile.module.scss';
+import reading from '../../../public/undraw_Book_reading_re_fu2c.png';
+import profilePic from '../../../public/undraw_Reading_book_re_kqpk.png';
+import FaceBookTwoToneIcon from '@mui/icons-material/FacebookTwoTone';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import PlaceIcon from '@mui/icons-material/Place';
+import LanguageIcon from '@mui/icons-material/Language';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Link from 'next/link';
+
+import Image from 'next/image';
+
+type Props = {};
+
+const profile = (props: Props) => {
+  return (
+    <>
+      <div className={s.profile}>
+        <div className={s.images}>
+          <Image src={reading} alt='cover pic' />
+          <Image src={profilePic} alt='profile pic' />
+        </div>
+        <div className={s.profileContainer}>
+          <div className={s.userInfo}>
+            <div className={s.left}>
+              <Link href='http://facebook.com'>
+                {/* make mui icons larger using fontSize = large */}
+                <FaceBookTwoToneIcon fontSize='large' />
+              </Link>
+              <Link href='http://facebook.com'>
+                <FaceBookTwoToneIcon fontSize='large' />
+              </Link>
+              <Link href='http://facebook.com'>
+                <FaceBookTwoToneIcon fontSize='large' />
+              </Link>
+              <Link href='http://facebook.com'>
+                <FaceBookTwoToneIcon fontSize='large' />
+              </Link>
+            </div>
+            <div className={s.center}>
+              <span>user's name</span>
+              <div className={s.info}>
+                <div className={s.item}>
+                  <PlaceIcon />
+                  <span>location</span>
+                </div>
+                <div className={s.item}>
+                  <LanguageIcon />
+                  <span>website here?</span>
+                </div>
+                <button>Follow</button>
+              </div>
+            </div>
+            <div className={s.right}>
+              <EmailOutlinedIcon />
+              <MoreVertIcon />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default profile;

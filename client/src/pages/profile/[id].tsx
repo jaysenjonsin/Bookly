969d067b920676992153a9ed3d651160545d8e1c
jaysenjonsin <100107689+jaysenjonsin@users.noreply.github.com>
2023-03-04 21:24:40 -0800

    Layout component (#24)
    * dark mode toggle working
    
    * add auth context
    
    * create stories, posts on feed
    
    * finish story UI, add posts
    
    * feed styling
    
    * merge main no conflicts
    
    * create comments component
    
    * finish styling comments
    
    * start styling profile
    
    * create layout component
    
    * bug: profile not rendering correctly

--- client/src/pages/profile/[id].tsx
+++ client/src/pages/profile/[id].tsx
@@ -13,55 +13,58 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Link from 'next/link';
 
 import Image from 'next/image';
+import Layout from '../../components/Layout';
 
 type Props = {};
 
 const profile = (props: Props) => {
   return (
     <>
-      <div className={s.profile}>
-        <div className={s.images}>
-          <Image src={reading} alt='cover pic' />
-          <Image src={profilePic} alt='profile pic' />
-        </div>
-        <div className={s.profileContainer}>
-          <div className={s.userInfo}>
-            <div className={s.left}>
-              <Link href='http://facebook.com'>
-                {/* make mui icons larger using fontSize = large */}
-                <FaceBookTwoToneIcon fontSize='large' />
-              </Link>
-              <Link href='http://facebook.com'>
-                <FaceBookTwoToneIcon fontSize='large' />
-              </Link>
-              <Link href='http://facebook.com'>
-                <FaceBookTwoToneIcon fontSize='large' />
-              </Link>
-              <Link href='http://facebook.com'>
-                <FaceBookTwoToneIcon fontSize='large' />
-              </Link>
-            </div>
-            <div className={s.center}>
-              <span>user's name</span>
-              <div className={s.info}>
-                <div className={s.item}>
-                  <PlaceIcon />
-                  <span>location</span>
-                </div>
-                <div className={s.item}>
-                  <LanguageIcon />
-                  <span>website here?</span>
-                </div>
-                <button>Follow</button>
+      <Layout>
+        <div className={s.profile}>
+          <div className={s.images}>
+            <Image src={reading} alt='cover pic' />
+            <Image src={profilePic} alt='profile pic' />
+          </div>
+          <div className={s.profileContainer}>
+            <div className={s.userInfo}>
+              <div className={s.left}>
+                <Link href='http://facebook.com'>
+                  {/* make mui icons larger using fontSize = large */}
+                  <FaceBookTwoToneIcon fontSize='large' />
+                </Link>
+                <Link href='http://facebook.com'>
+                  <FaceBookTwoToneIcon fontSize='large' />
+                </Link>
+                <Link href='http://facebook.com'>
+                  <FaceBookTwoToneIcon fontSize='large' />
+                </Link>
+                <Link href='http://facebook.com'>
+                  <FaceBookTwoToneIcon fontSize='large' />
+                </Link>
               </div>
-            </div>
-            <div className={s.right}>
-              <EmailOutlinedIcon />
-              <MoreVertIcon />
+              <div className={s.center}>
+                <span>user's name</span>
+                <div className={s.info}>
+                  <div className={s.item}>
+                    <PlaceIcon />
+                    <span>location</span>
+                  </div>
+                  <div className={s.item}>
+                    <LanguageIcon />
+                    <span>website here?</span>
+                  </div>
+                  <button>Follow</button>
+                </div>
+              </div>
+              <div className={s.right}>
+                <EmailOutlinedIcon />
+                <MoreVertIcon />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Layout>
     </>
   );
 };
